1cb030f360781ffb3cbe9f127b474cfcee1a5ba1
Vladimir Gonchar <goncharvell@gmail.com>
2017-01-10 23:12:18 +0200

    Added facet search

--- app.js
+++ app.js
@@ -3,15 +3,29 @@ $(document).ready(function(){
 
         $searchField = $(".search-field"),
         $searchSubmit = $(".search-submit"),
-        $searchResultsContainer = $(".search-results");
+        $searchResultsContainer = $(".search-results"),
+        $facetsContainer = $(".fasets");
 
     $searchSubmit.on("click", function(){
         var searchText = $searchField.val();
         if(searchText){
             SparqlSearchService.search(searchText).
                 then(renderSearchResults);
+            SparqlSearchService.searchFacets(searchText).
+                then(renderFacets);
         }
-    })
+    });
+
+    $facetsContainer.on("click", ".facet", function(e){
+        e.preventDefault();
+        var facetIri = $(e.target).attr("href");
+        var searchText = $searchField.val();
+
+        SparqlSearchService.search(searchText, facetIri).
+            then(renderSearchResults);
+        SparqlSearchService.searchFacets(searchText, facetIri).
+            then(renderFacets);
+    });
 
     function renderSearchResults(response, status){
         $searchResultsContainer.empty();
@@ -45,6 +59,18 @@ $(document).ready(function(){
         });
     };
 
+    function renderFacets(response){
+        $facetsContainer.empty();
+
+        response.results.bindings.forEach(function(item){
+            $("<a/>", {
+                class: "facet",
+                href: item.type.value,
+                text: item.type.value
+            }).appendTo($facetsContainer);
+        });
+    };
+
     function trimText(text){
         if(text.length > MAX_TEXT_LENGTH){
             return text.slice(0, MAX_TEXT_LENGTH) + "...";
@@ -59,14 +85,19 @@ var SparqlSearchService = {
 
     SEARCH_LIMIT_PER_REPO: 10,
 
-    search: function(searchText){
+    search: function(searchText, facet){
 
+        var facetTriplet = "";
+        if(facet){
+            facetTriplet = " ?x a <" + facet + ">."
+        }
         return $.ajax("https://data.open.ac.uk/sparql", {
             dataType: "json",
             crossDomain: true,
             data: "query=SELECT ?x ?url (GROUP_CONCAT(?type) as ?types) ?description "
                  + " WHERE {"
                  + " ?x <http://purl.org/dc/terms/description> ?description."
+                 + facetTriplet
                  + " ?x a ?type."
                  + " OPTIONAL {?x <http://dbpedia.org/property/url> ?url}."
                  + " FILTER REGEX(?description, '" + searchText + "')."
@@ -74,9 +105,34 @@ var SparqlSearchService = {
                  + " group by ?x ?description ?url limit " + SparqlSearchService.SEARCH_LIMIT_PER_REPO,
             processData: false,
             error: function(jqXHR, status, error){
-                debugger;
+                console.error(error);
             }
         })
 
+    },
+
+    searchFacets: function(searchText, facet){
+        var facetTriplet = "";
+        if(facet){
+            facetTriplet = " ?x a <" + facet + ">."
+        }
+        return $.ajax("https://data.open.ac.uk/sparql", {
+            dataType: "json",
+            crossDomain: true,
+            data: "query=SELECT ?type (COUNT(?x) as ?amount) "
+                 + " WHERE {"
+                 + " ?x <http://purl.org/dc/terms/description> ?description."
+                 + " ?x a ?type."
+                 + facetTriplet
+                 + " FILTER REGEX(?description, '" + searchText + "')."
+                 + "}"
+                 + " GROUP BY ?type "
+                 + " ORDER BY DESC(?amount)"
+,
+            processData: false,
+            error: function(jqXHR, status, error){
+                console.error(error);
+            }
+        })
     }
 }
